95a51f4aee5197df59e21367d665be22a598b54a
cdacatcnashik <72591075+cdacatcnashik@users.noreply.github.com>
2021-11-10 22:21:52 +0530

    Add files via upload

--- Java/Basics/PrimitiveTypeTest/Program.java
+++ Java/Basics/PrimitiveTypeTest/Program.java
@@ -1,1 +1,17 @@
+class Program {
+
+	private static double income(double sum, int years, boolean risky) {
+		float rate = risky ? 8 : 5;
+		double amount = sum * Math.pow(1 + rate / 100, years);
+		return amount - sum;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome Investor");
+		double s = Double.parseDouble(args[0]);
+		int y = Integer.parseInt(args[1]);
+		System.out.printf("Income in risk-free investment: %.2f%n", income(s, y, false));
+		System.out.printf("Income in riskful investment: %.2f%n", income(s, y, true));
+	}
+}
 
