b8715ccad21976a6728055e0e497a21cb7bfcb66
cdacatcnashik <72591075+cdacatcnashik@users.noreply.github.com>
2021-11-16 10:08:17 +0530

    Add files via upload

--- Java/Inheritance/InterfaceTest2/src/Dealer.java
+++ Java/Inheritance/InterfaceTest2/src/Dealer.java
@@ -1,1 +1,13 @@
+package taxation;
+
+public class Dealer implements TaxPayer {
+
+	private double totalSales;
+
+	public Dealer(double sales) { totalSales = sales; }
+
+	public double annualIncome() {
+		return 0.125 * totalSales;
+	}
+}
 
