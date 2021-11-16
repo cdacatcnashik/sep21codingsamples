fa8168876c2eaae0321feab4af26a90ab9e79071
cdacatcnashik <72591075+cdacatcnashik@users.noreply.github.com>
2021-11-16 09:56:21 +0530

    Add files via upload

--- Java/Generics/SetTest/Program.java
+++ Java/Generics/SetTest/Program.java
@@ -1,1 +1,17 @@
+import java.util.*;
 
+class Program {
+
+	public static void main(String[] args) {
+		//Set<Interval> store = new HashSet<>();
+		Set<Interval> store = new TreeSet<>();
+		store.add(new Interval(5, 31));
+		store.add(new Interval(4, 12));
+		store.add(new Interval(2, 43));
+		store.add(new Interval(7, 24));
+		store.add(new Interval(6, 55));
+		store.add(new Interval(3, 72));
+		for(var item : store)
+			System.out.println(item);
+	}
+}
