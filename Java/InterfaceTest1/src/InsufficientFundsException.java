4f21a93b4d2e135d0d25465bdb2b969533e335b6
cdacatcnashik <72591075+cdacatcnashik@users.noreply.github.com>
2021-11-16 10:01:29 +0530

    Delete Java/InterfaceTest1 directory

--- Java/InterfaceTest1/src/InsufficientFundsException.java
+++
@@ -1,6 +0,0 @@
-package banking;
-
-//compile-time checked exception can only occur within a body of a method in which
-//it is declared to be thrown or within a try block which catches it
-public class InsufficientFundsException extends Exception {}
-
--- Java/InterfaceTest1/src/Profitable.java
+++
@@ -1,9 +0,0 @@
-package banking;
-
-public interface Profitable {
-
-	float MIN_RATE = 4;
-
-	double interest(int months);
-}
-
