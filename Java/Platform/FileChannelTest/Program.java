251336838095fdcc0e7644ccf26bdf8f3cc6fcd9
cdacatcnashik <72591075+cdacatcnashik@users.noreply.github.com>
2021-11-20 11:35:25 +0530

    Add files via upload

--- Java/Platform/FileChannelTest/Program.java
+++ Java/Platform/FileChannelTest/Program.java
@@ -1,1 +1,18 @@
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+class Program {
+
+	public static void main(String[] args) throws Exception {
+		var file = Path.of(args[0]);
+		var channel = FileChannel.open(file, StandardOpenOption.READ, StandardOpenOption.WRITE);
+		var lock = channel.lock();
+		int n = (int)channel.size();
+		var buffer = channel.map(FileChannel.MapMode.READ_WRITE, 0, n);
+		Transformer.transform(buffer, n);
+		lock.release();
+		channel.close();
+	}
+}
 
