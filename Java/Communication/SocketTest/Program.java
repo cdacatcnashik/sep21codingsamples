f00a324747e35d356100d4b1ad322da1411ea7d3
cdacatcnashik <72591075+cdacatcnashik@users.noreply.github.com>
2021-11-23 11:32:18 +0530

    Add files via upload

--- Java/Communication/SocketTest/Program.java
+++ Java/Communication/SocketTest/Program.java
@@ -1,1 +1,35 @@
+import java.io.*;
+import java.net.*;
+
+class Program {
+
+	public static void main(String[] args) throws Exception {
+		String item = args[0].toLowerCase();
+		int quantity = Integer.parseInt(args[1]);
+		String host = args.length > 2 ? args[2] : "localhost";
+		var server = new Socket(host, 4000);
+		var input = server.getInputStream();
+		var reader = new BufferedReader(new InputStreamReader(input));
+		var output = server.getOutputStream();
+		var writer = new PrintWriter(new OutputStreamWriter(output));
+		System.out.println(reader.readLine());
+		writer.println(item);
+		writer.flush();
+		String text = reader.readLine();
+		writer.close();
+		reader.close();
+		server.close();
+		if(text != null){
+			String[] info = text.split("&");
+			double price = Double.parseDouble(info[0].substring(6));
+			int stock = Integer.parseInt(info[1].substring(6));
+			if(quantity <= stock)
+				System.out.printf("Total Payment: %.2f%n", quantity * price);
+			else
+				System.out.println("Out of stock!");
+		}else{
+			System.out.println("Not available!");
+		}
+	}
+}
 
