81209ee073a1100763e030dc8df478996c463a53
cdacatcnashik <72591075+cdacatcnashik@users.noreply.github.com>
2021-11-23 11:34:29 +0530

    Add files via upload

--- Java/Communication/DatagramSocketTest/Program.java
+++ Java/Communication/DatagramSocketTest/Program.java
@@ -1,1 +1,22 @@
+import java.net.*;
+import java.util.*;
+
+class Program {
+
+	public static void main(String[] args) throws Exception {
+		String[] symbols = {"APPL", "GOGL", "INTC", "MSFT", "ORCL"};
+		var rdm = new Random();
+		var publisher = new DatagramSocket();
+		var group = InetAddress.getByName("230.0.0.1"); //class-D address (224-239.*.*.*)
+		for(;;){
+			int i = rdm.nextInt(symbols.length);
+			double p = 0.01 * (1000 + rdm.nextInt(9000));
+			String message = String.format("%s - %.2f", symbols[i], p);
+			var packet = new DatagramPacket(message.getBytes(), message.length(), group, 4001);
+			publisher.send(packet);
+			Thread.sleep(5000);
+		}
+	}
+
+}
 
