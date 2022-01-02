8b6f50035b0d524ee136514ce8ac1a55f62414b7
cdacatcnashik <72591075+cdacatcnashik@users.noreply.github.com>
2022-01-02 17:12:53 +0530

    Add files via upload

---
+++ DotNet/Generics/ListTest/DemoApp/Program.cs
@@ -0,0 +1,11 @@
+﻿List<Interval> store = new List<Interval>();
+store.Add(new Interval(4, 31));
+store.Add(new Interval(7, 42));
+store.Add(new Interval(5, 13));
+store.Add(new Interval(6, 54));
+store.Add(new Interval(3, 25));
+store.Add(new Interval(4, 73));
+store.Sort();
+foreach(var i in store)
+    Console.WriteLine(i);
+Console.WriteLine("Third Interval = {0}", store[2]);
