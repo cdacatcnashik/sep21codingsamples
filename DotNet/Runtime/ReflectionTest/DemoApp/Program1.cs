e739d8f88fc0eaa30bad92961a3394ea64fd3b52
cdacatcnashik <72591075+cdacatcnashik@users.noreply.github.com>
2022-01-03 14:22:14 +0530

    Add files via upload

--- DotNet/Runtime/ReflectionTest/DemoApp/Program1.cs
+++ DotNet/Runtime/ReflectionTest/DemoApp/Program1.cs
@@ -1,1 +1,32 @@
+﻿record Student (string Name, string Course, float Score);
 
+record Teacher (string Name, int Experience);
+
+class Program
+{
+    private static void PrintAsXml(object info)
+    {
+        Console.WriteLine("<Student>");
+        Console.WriteLine("  <Name>{0}</Name>", info.Name);
+        Console.WriteLine("  <Course>{0}</Course>", info.Course);
+        Console.WriteLine("  <Score>{0}</Score>", info.Score);
+        Console.WriteLine("</Student>");
+        Console.WriteLine();
+    }
+
+    private static void PrintAsXml(Teacher info)
+    {
+        Console.WriteLine("<Teacher>");
+        Console.WriteLine("  <Name>{0}</Name>", info.Name);
+        Console.WriteLine("  <Experience>{0}</Experience>", info.Experience);
+        Console.WriteLine("</Teacher>");
+        Console.WriteLine();
+    }
+
+
+    public static void Main()
+    {
+        PrintAsXml(new Student("Jack", "Java", 67));
+        PrintAsXml(new Teacher("Jill", 9));
+    }
+}
---
+++ DotNet/Runtime/ReflectionTest/DemoApp/Program2.cs
@@ -0,0 +1,22 @@
+﻿record Student (string Name, string Course, float Score);
+
+record Teacher (string Name, int Experience);
+
+class Program
+{
+    private static void PrintAsXml(object info)
+    {
+        Type t = info.GetType();
+        Console.WriteLine("<{0}>", t.Name);
+        foreach(var prop in t.GetProperties())
+            Console.WriteLine("  <{0}>{1}</{0}>", prop.Name, prop.GetValue(info));
+        Console.WriteLine("</{0}>", t.Name);
+        Console.WriteLine();
+    }
+
+    public static void Main()
+    {
+        PrintAsXml(new Student("Jack", "Java", 67));
+        PrintAsXml(new Teacher("Jill", 9));
+    }
+}
